10815223d11947be37475baf3b7eaf72a68e3e5d
h2ash <hun22ash@gmail.com>
2019-08-13 00:03:51 +0300

    Updates

--- precache-manifest.bc20ed8df6350ca9380e68e610766357.js
+++ precache-manifest.bc20ed8df6350ca9380e68e610766357.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bb7b8ebfaa1ca41fb36a8772d12eeda3",
+    "revision": "faf4a8c8dd8baa4d428ca836f934aa19",
     "url": "/react_phone-catalog/index.html"
   },
   {
-    "revision": "1882249135394920b858",
+    "revision": "96acdb01aef8d7df8828",
     "url": "/react_phone-catalog/static/css/main.b66bf530.chunk.css"
   },
   {
-    "revision": "f6f98bee22722a7b131d",
-    "url": "/react_phone-catalog/static/js/2.29e18897.chunk.js"
+    "revision": "35c7349d3b9f8a5f261d",
+    "url": "/react_phone-catalog/static/js/2.d99222e6.chunk.js"
   },
   {
-    "revision": "1882249135394920b858",
-    "url": "/react_phone-catalog/static/js/main.21840578.chunk.js"
+    "revision": "96acdb01aef8d7df8828",
+    "url": "/react_phone-catalog/static/js/main.8b5445c5.chunk.js"
   },
   {
     "revision": "9b3e1d68b9cc1dd9a925",
